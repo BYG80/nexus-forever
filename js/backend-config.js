d579ba39d1811ee4e83f39fb3b7c5064112d0e62
BYG80 <gumersantfdz@gmail.com>
2026-09-16 22:41:29 +0200

    Delete js directory

--- js/backend-config.js
+++
@@ -1,3 +0,0 @@
-// URL del backend. En local: http://localhost:3000
-// En producción, cambia este valor por la URL HTTPS de tu API.
-window.NEXUS_API_URL = window.NEXUS_API_URL || "http://localhost:3000";
